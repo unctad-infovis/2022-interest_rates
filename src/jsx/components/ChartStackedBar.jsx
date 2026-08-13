37a904ac0ee5fdadef4c4b8726c441c968507af4
Teemo Tebest <teemo.tebest@gmail.com>
2026-08-14 00:54:42 +0300

    Host UNCTAD logo locally instead of hardcoded unctad.org URL (blocks downloads on localhost)

--- src/jsx/components/ChartStackedBar.jsx
+++ src/jsx/components/ChartStackedBar.jsx
@@ -70,7 +70,7 @@ function StackedBarChart({
         events: {
           load() {
             // eslint-disable-next-line react/no-this-in-sfc
-            this.renderer.image('https://unctad.org/sites/default/files/2022-11/unctad_logo.svg', 5, 15, 80, 100).add();
+            this.renderer.image(`${window.location.href.includes('unctad.org') ? 'https://storage.unctad.org/2022-interest_rates/' : './'}assets/img/unctad_logo.svg`, 5, 15, 80, 100).add();
           }
         },
         height: chartHeight,
